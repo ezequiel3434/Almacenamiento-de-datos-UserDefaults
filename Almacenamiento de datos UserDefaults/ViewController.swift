d9a95779cdeff59d875e07738875235461c7d9ae
Ezequiel <ezequiel3434@gmail.com>
2020-06-29 18:12:41 -0300

    codificación y prueba del codigo

--- Almacenamiento de datos UserDefaults/ViewController.swift
+++ Almacenamiento de datos UserDefaults/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let kMyKey = "MY_KEY"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+// MARK: UserDefaults
+    
+    @IBAction func getButtonAction(_ sender: Any) {
+        if let value = UserDefaults.standard.string(forKey: kMyKey) {
+            showAlert(message: value)
+        } else {
+            showAlert(message: "No hay un valor para esta clave")
+        }
+    }
+    
+    @IBAction func putButtonAction(_ sender: Any) {
+        
+        UserDefaults.standard.set("Hola", forKey: kMyKey)
+        UserDefaults.standard.synchronize()
+        showAlert(message: "Hemos guardado un valor")
+    }
+    @IBAction func deleteButtonAction(_ sender: Any) {
+        
+        UserDefaults.standard.removeObject(forKey: kMyKey)
+        UserDefaults.standard.synchronize()
+        showAlert(message: "Hemos borrado un valor")
+    }
+    
+    private func showAlert(message: String){
+        let alert = UIAlertController(title: "MyUserDefaults", message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
 
